0e2f167141a63236facc747e86582191d45c17a7
tclos <93877648+tclos@users.noreply.github.com>
2026-02-04 17:27:49 -0300

    added 2d fno

--- fno_diffusion/model.py
+++ fno_diffusion/model.py
@@ -11,3 +11,12 @@ def make_fno_1d(n_modes=16, hidden_channels=64, in_channels=1, out_channels=1):
         out_channels=out_channels,
     )
     return model
+
+def make_fno_2d(n_modes=(16, 16), hidden_channels=64, in_channels=1, out_channels=1):
+    model = FNO(
+        n_modes=n_modes,          # (n_modes_omega, n_modes_theta)
+        hidden_channels=hidden_channels,
+        in_channels=in_channels,
+        out_channels=out_channels,
+    )
+    return model
