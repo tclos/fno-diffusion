7eced1b26e3e35662d4f56bb53f5eaf84907e645
tclos <93877648+tclos@users.noreply.github.com>
2026-03-09 20:00:40 -0300

    Update model.py

--- fno_diffusion/model.py
+++ fno_diffusion/model.py
@@ -2,19 +2,9 @@
 
 from neuralop.models import FNO
 
-
-def make_fno_1d(n_modes=16, hidden_channels=64, in_channels=1, out_channels=1):
-    model = FNO(
-        n_modes=(n_modes,),
-        hidden_channels=hidden_channels,
-        in_channels=in_channels,
-        out_channels=out_channels,
-    )
-    return model
-
 def make_fno_2d(n_modes=(16, 16), hidden_channels=64, in_channels=1, out_channels=1):
     model = FNO(
-        n_modes=n_modes,          # (n_modes_omega, n_modes_theta)
+        n_modes=n_modes,          
         hidden_channels=hidden_channels,
         in_channels=in_channels,
         out_channels=out_channels,
